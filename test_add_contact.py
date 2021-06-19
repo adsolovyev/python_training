1e60b0e565cdb9efbfca8f3225adaade015c95fd
anreysolovyev <86119184+anreysolovyev@users.noreply.github.com>
2021-06-19 18:19:25 +0300

    new test added - add contact (short form) major fixes

--- test_add_contact.py
+++ test_add_contact.py
@@ -12,29 +12,42 @@ class TestAddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
     
-    def test_add_group(self):
+    def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_add_new_page(wd)
-        self.add_firstname_middlename_lastname(wd, Fullname(name="name1", middlename="middlename1", lastname="lastname1"))
-        self.add_nickname(wd)
-        self.add_title(wd)
-        self.add_company(wd)
-        self.add_address(wd)
-        self.add_home(wd)
-        self.add_mobile(wd)
-        self.add_work(wd)
-        self.add_fax(wd)
-        self.add_email(wd)
-        self.add_email2(wd)
-        self.add_email3(wd)
-        self.add_homepage(wd)
+        self.add_firstname_middlename_lastname(wd, Fullname(name="name_001", middlename="middlename_001", lastname="lastname_001"))
+        self.add_nickname(wd, nickname="test_nickname")
+        self.add_title(wd, title="test_title")
+        self.add_company(wd, company="company_name")
+        self.add_address(wd, address="test street, 1, 1")
+        self.add_home(wd, home="home sweet home")
+        self.add_mobile(wd, mobile="+0 000 000 0000")
+        self.add_work(wd, work="work work work")
+        self.add_fax(wd, fax="+1 111 111 1111")
+        self.add_email(wd, email="email@1")
+        self.add_email2(wd, email="email@2")
+        self.add_email3(wd, email="email@3")
+        self.add_homepage(wd, homepage="www.homepage.com")
         self.add_birthday_info(wd)
         self.add_aniversary_info(wd)
-        self.add_second_address(wd)
-        self.add_second_phone(wd)
-        self.add_notes(wd)
+        self.add_second_address(wd, secondaryaddress="address #2")
+        self.add_second_phone(wd, secondaryhome="home #2")
+        self.add_notes(wd, notes="notes notes notes")
+        self.submit_new_contact(wd)
+        self.go_to_home_page(wd)
+        self.logout(wd)
+
+    def test_add_contact_short_form(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_add_new_page(wd)
+        self.add_firstname_middlename_lastname(wd, Fullname(name="name_001", middlename="middlename_001", lastname="lastname_001"))
+        self.add_nickname(wd, nickname="test_nickname")
+        self.add_email(wd, email="email@1")
+        self.add_birthday_info(wd)
         self.submit_new_contact(wd)
         self.go_to_home_page(wd)
         self.logout(wd)
@@ -48,20 +61,20 @@ class TestAddGroup(unittest.TestCase):
     def submit_new_contact(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def add_notes(self, wd):
+    def add_notes(self, wd, notes):
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("notes1")
+        wd.find_element_by_name("notes").send_keys(notes)
 
-    def add_second_phone(self, wd):
+    def add_second_phone(self, wd, secondaryhome):
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("secondaryhome1")
+        wd.find_element_by_name("phone2").send_keys(secondaryhome)
 
-    def add_second_address(self, wd):
+    def add_second_address(self, wd, secondaryaddress):
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("secondaryaddress1")
+        wd.find_element_by_name("address2").send_keys(secondaryaddress)
 
     def add_aniversary_info(self, wd):
         # add day
@@ -91,65 +104,65 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("2000")
 
-    def add_homepage(self, wd):
+    def add_homepage(self, wd, homepage):
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys("homepage")
+        wd.find_element_by_name("homepage").send_keys(homepage)
 
-    def add_email3(self, wd):
+    def add_email3(self, wd, email):
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("email3")
+        wd.find_element_by_name("email3").send_keys(email)
 
-    def add_email2(self, wd):
+    def add_email2(self, wd, email):
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("email2")
+        wd.find_element_by_name("email2").send_keys(email)
 
-    def add_email(self, wd):
+    def add_email(self, wd, email):
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("email1")
+        wd.find_element_by_name("email").send_keys(email)
 
-    def add_fax(self, wd):
+    def add_fax(self, wd, fax):
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("fax1")
+        wd.find_element_by_name("fax").send_keys(fax)
 
-    def add_work(self, wd):
+    def add_work(self, wd, work):
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("work1")
+        wd.find_element_by_name("work").send_keys(work)
 
-    def add_mobile(self, wd):
+    def add_mobile(self, wd, mobile):
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("mobile1")
+        wd.find_element_by_name("mobile").send_keys(mobile)
 
-    def add_home(self, wd):
+    def add_home(self, wd, home):
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("home1")
+        wd.find_element_by_name("home").send_keys(home)
 
-    def add_address(self, wd):
+    def add_address(self, wd, address):
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("address1")
+        wd.find_element_by_name("address").send_keys(address)
 
-    def add_company(self, wd):
+    def add_company(self, wd, company):
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("company1")
+        wd.find_element_by_name("company").send_keys(company)
 
-    def add_title(self, wd):
+    def add_title(self, wd, title):
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("title1")
+        wd.find_element_by_name("title").send_keys(title)
 
-    def add_nickname(self, wd):
+    def add_nickname(self, wd, nickname):
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("nickname")
+        wd.find_element_by_name("nickname").send_keys(nickname)
 
     def add_firstname_middlename_lastname(self, wd, fullname):
         # add firstname, middlename, lastname
@@ -193,19 +206,7 @@ class TestAddGroup(unittest.TestCase):
         except NoAlertPresentException as e:
             return False
         return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.wd.quit()
 
