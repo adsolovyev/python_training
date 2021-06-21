a089536070a00e21a86620044a14e2b2c8e58863
anreysolovyev <86119184+anreysolovyev@users.noreply.github.com>
2021-06-21 20:34:15 +0300

    conftest created, browser usage updated

--- fixture/group.py
+++ fixture/group.py
@@ -9,7 +9,6 @@ class GroupHelper:
 
     def create(self, group):
         wd = self.app.wd
-        # open groups page
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
@@ -25,6 +24,16 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        # go to groups page
+        self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
         # go to groups page
         self.return_to_groups_page()
 
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
-import pytest
+
 from model.fullname import Fullname
-from fixture.application import Application
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-    
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Fullname(name="name_001", middlename="middlename_001", lastname="lastname_001"), nickname="test_nickname", email="email@1.long")
