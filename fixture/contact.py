22d00da08fbd78be772e1ab5eab6a81ef11f8cc6
anreysolovyev <86119184+anreysolovyev@users.noreply.github.com>
2021-06-22 13:17:23 +0300

    added test_edit_1st_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -52,6 +52,40 @@ class ContactHelper:
         # go to home page
         self.open_home_page()
 
+    def edit_first_contact(self, fullname, nickname, email):
+        wd = self.app.wd
+        self.open_home_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # press edit button
+        wd.find_element_by_name("edit").click()
+        # add firstname, middlename, lastname
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(fullname.name)
+        # add middlename
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(fullname.middlename)
+        # add lastname
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(fullname.lastname)
+        # add nickname
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(nickname)
+        # add email
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(email)
+        # submit update
+        wd.find_element_by_name("update").click()
+        # go to groups page
+        self.open_home_page()
+
+
+
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,11 +5,13 @@ from model.fullname import Fullname
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create(Fullname(name="name_001", middlename="middlename_001", lastname="lastname_001"), nickname="test_nickname", email="email@1.long")
+    app.contact.create(Fullname(name="name_001", middlename="middlename_001", lastname="lastname_001"),
+                       nickname="test_nickname", email="email@1.long")
     app.session.logout()
 
 
 def test_add_contact_short_form(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create(Fullname(name="name_002", middlename="middlename_002", lastname="lastname_002"), nickname="test_short_nickname", email="email@1.short")
+    app.contact.create(Fullname(name="name_002", middlename="middlename_002", lastname="lastname_002"),
+                       nickname="test_short_nickname", email="email@1.short")
     app.session.logout()
