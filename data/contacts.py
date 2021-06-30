f69504acc8a0f7641e9ecc56bd6d8b5cb667ffca
anreysolovyev <86119184+anreysolovyev@users.noreply.github.com>
2021-06-30 15:04:06 +0300

    dynamic tests generation

---
+++ data/contacts.py
@@ -0,0 +1,7 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname='firstname1', lastname='lastname1', email='email@1', mobilephone='0123456789'),
+    Contact(firstname='firstname2', lastname='lastname2', email='email@2', mobilephone='1234567890'),
+]
